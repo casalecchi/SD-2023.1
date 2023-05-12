d6705d174183a1128e5127057579caa26bdbda06
Felipe Casalecchi <felipe.casalecchi@poli.ufrj.br>
2023-05-12 13:26:59 -0300

    counter spinlocks changed

--- TP2/spinlocks/counter.c
+++ TP2/spinlocks/counter.c
@@ -6,77 +6,52 @@
 #include <pthread.h>
 
 
-int delta() {
+int buffer[100000];
+int N = 100000;
+int K = 2;
+
+int delta(int i) {
     // Definir a seed baseada no tempo
-    srand(time(NULL));
+    srand(time(NULL) * i);
 
     // Gerar número entre 1 e 100
     int number = rand() % 201 - 100;
     return number;
 }
 
-void *routine(void *arg) {
-    int value = rand() % 6 + 1;
-    int *result = malloc(sizeof(int));
-    *result = value;
-    printf("Create %d\n", value);
-    return result;
+void fill_buffer() {
+    for (int i = 0; i < N; i++) {
+        int n = delta(i);
+        printf("iteração: %d\n", i);
+        buffer[i] = n;
+    }
+}
+
+void *thread_sum(void *arg) {
+    int thread_num = *(int*) arg;
+    int size = N / K;
+    int th_counter = 0;
+    for (int i = 0; i < size; i++) {
+        th_counter += buffer[i + thread_num * size];
+    }
+    return NULL;
 }
 
 
 int main(int argc, char **argv) {
-    double power;
-    int N;
-    int K;
     pthread_t threads[K];
 
-    K = atoi(argv[1]);
-
-    printf("Choose the power of 10 for buffer size [7 | 8 | 9]: ");
-    scanf("%lf", &power);
-    N = pow(10.0, power);
-    printf("Buffer size = %d\n", N);
-
-    // int *buffer = malloc (sizeof(int)*N);
+    fill_buffer();
 
     for (int i = 0; i < K; i++) {
-        if (pthread_create(&threads[i], NULL, routine, NULL) != 0) {
+        if (pthread_create(&threads[i], NULL, *fill_buffer, NULL) != 0) {
             printf("Error creating thread %d\n", i);
         }
     }
 
-    int *result;
-
     for (int i = 0; i < K; i++) {
-        if (pthread_join(threads[i], (void **) &result) != 0) {
+        if (pthread_join(threads[i], NULL) != 0) {
             printf("Cannot wait for thread %d\n", i);
         }
     }
-
-    printf("Result : %p\n", &result);
-    free(result);
-    
-    // time_t start_time = time(NULL);
-
-    // for (int i = 0; i < N; i++) {
-    //     int n = delta();
-    //     printf("iteração: %d\n", i);
-    //     buffer[i] = n;
-    // }
-
-    // time_t end_time = time(NULL);
-    // printf("Tempo para preencher o vetor: %lds\n", end_time - start_time);
-
-    // start_time = time(NULL);
-    // int counter = 0;
-    // for (int i = 0; i < N; i++) {
-    //     counter += buffer[i];
-    // }
-    // end_time = time(NULL);
-
-    // printf("Tempo para somar o vetor: %lds\n", end_time - start_time);
-    // printf("%d\n", counter);
-
-    // free(buffer);
-    return 0;
 }
