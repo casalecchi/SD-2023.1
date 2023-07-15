cd3de2b3c64b3382399c2e208687784f405677e2
casalecchi <felipevilela191@gmail.com>
2023-07-15 19:35:26 -0300

    coordinator and client still failing

---
+++ TP3/client.py
@@ -0,0 +1,45 @@
+import socket
+import time
+import os
+from messages import *
+
+def client_process(coordinator_address, coordinator_port, k, r, process_id):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+
+    # conectar ao coordenador
+    
+
+    for i in range(r):
+        sock.connect((coordinator_address, coordinator_port))
+        # Envie uma mensagem de solicitação ao coordenador
+        request_message = create_request_message(process_id, 10)
+        sock.send(request_message.encode())
+        print(f"Requisição do {request_message}")
+        
+
+        # Aguarde a resposta do coordenador
+        response = sock.recv(10)
+        print(f"Liberação do coordenador: {response}")
+
+        message_type, _, _ = response.decode().split("|")
+
+        # Verifique se o acesso foi concedido (verificar a mensagem de resposta)
+        if message_type == '2':
+            # Acesso concedido, execute as operações na região crítica
+            with open("resultado.txt", "a") as file:
+                current_time = time.time()
+                file.write(f"{process_id} - {current_time}\n")
+
+        # Aguarde k segundos antes de fazer a próxima solicitação
+        time.sleep(k)
+
+        # Se conecta novamente, para mandar outra mensagem
+        release_message = create_release_message(process_id, 10)
+        sock.send(release_message.encode())
+        print(f"Saindo da RC... {release_message}")
+        
+
+    # Encerre o socket
+    sock.close()
+
+client_process("localhost", 8001, 2, 5, os.getpid())
---
+++ TP3/coordinator.py
@@ -0,0 +1,86 @@
+import threading
+import socket
+import os
+from queue import Queue
+from messages import *
+
+MESSAGE_SIZE = 10
+semaphore = threading.Semaphore(1)
+request_queue = Queue()
+access_stats = {}
+
+# Crie um socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Vincule o socket a um endereço e porta
+server_address = ("localhost", 8001)
+sock.bind(server_address)
+sock.listen(100)
+print("Socket criado")
+
+def terminal_interface():
+    while True:
+        command = input("Digite o comando: ")
+
+        if command == "fila":
+            print(request_queue.queue)
+        elif command == "estatisticas":
+            print(access_stats)
+        elif command == "encerrar":
+            # Encerra a execução do coordenador
+            sock.close()
+            os._exit(0)
+        else:
+            print("Comando inválido")
+
+
+# Criar threads e recebr pedidos
+def receive_requests():
+    while True:
+        print("Esperando cliente...")
+        client, _ = sock.accept()
+        print("Cliente aceito...")
+        data = client.recv(10)
+        print(f"Dado recebido: {data}")
+
+        # Processar o pedido recebido
+        semaphore.acquire()
+        request_queue.put(data.decode())
+        semaphore.release()
+
+        # Registrar o pedido no arquivo de log
+        # log_message = create_log_message(data.decode(), address)
+        # write_to_log(log_message)
+
+        process_thread = threading.Thread(target=process_request, args=(client,))
+        process_thread.start()
+
+
+def process_request(client):
+    print("Chegou mensagem")
+    semaphore.acquire()
+    request = request_queue.get()
+    semaphore.release()
+
+    # Extrair informações do pedido (identificador da mensagem, identificador do processo)
+    message_type, process_id, _ = request.split("|")
+
+    # Tomar decisões com base no pedido recebido (conceder acesso, atualizar estatísticas, etc.)
+    if message_type == '1':
+        grant_message = create_grant_message(process_id, 10)
+        client.send(grant_message.encode())
+        print(f"Mensagem de permissão enviada a {process_id}")
+
+    msg = client.recv(10)
+    print(msg)
+    release, pid, _ = msg.decode().split("|")
+
+    if release == '3':
+        print(f"Mensagem de liberação de RC recebida {pid}")
+
+
+terminal_thread = threading.Thread(target=terminal_interface)
+receive_thread = threading.Thread(target=receive_requests)
+
+terminal_thread.start()
+receive_thread.start()
