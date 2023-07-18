b4ed080d8ffd7c7bf7cabbc9d3dd1672f805c809
casalecchi <felipevilela191@gmail.com>
2023-07-18 10:04:07 -0300

    coordinator generating valid log - finally

--- TP3/coordinator.py
+++ TP3/coordinator.py
@@ -3,6 +3,7 @@ import socket
 import threading
 from collections import deque
 from utils import *
+import time
 
 
 # constants
@@ -17,8 +18,9 @@ messages_queue = deque()
 access_stats = {}
 # semaphores
 queue_sem = threading.Semaphore(1)
-critical_sem = threading.Semaphore(1)
+critical_sem = threading.Lock()
 log_sem = threading.Semaphore(1)
+release_lock = threading.Lock()
 
 
 # terminal thread
@@ -57,53 +59,61 @@ def receive_messages():
         # put message on queue and write on log
         queue_sem.acquire()
         log_sem.acquire()
-
-        write_to_log(data, log_file)
-        messages_queue.append(data)
         
-        log_sem.release()
+        release_flag = False
+        write_to_log(data, log_file)
+
+        if data.decode()[0] == "3":
+            release_flag = True
+        else:
+            messages_queue.append(data)
+            
         queue_sem.release()
+        log_sem.release()
+        
 
         # create new thread to process new message
-        process_thread = threading.Thread(target=process_request, args=(client,))
+        process_thread = threading.Thread(target=process_request, args=(client,release_flag,data))
         process_thread.start()
 
     sock.close()
 
 
 # thread to process messages
-def process_request(client: socket):
+def process_request(client: socket, release_flag: bool, release_message: bytes):
     global log_sem, queue_sem, critical_sem
 
+    if release_flag:
+        release_lock.acquire()
+        _, pid = decode_message(release_message)
+        # compute statistics
+        access_stats[pid] = access_stats.get(pid, 0) + 1
+        # free critical section
+        release_lock.release()
+        critical_sem.release()
+        return
+
     # read the next message on queue
+    critical_sem.acquire()
+
     queue_sem.acquire()
     next_message = messages_queue.popleft()
     queue_sem.release()
 
     message_type, pid = decode_message(next_message)
 
-    # process the REQUEST message
-    if message_type == 1:
-        # create the permission message
-        grant_message = create_grant_message(pid, MESSAGE_SIZE)
-        data = grant_message.encode()
+    # create the permission message
+    grant_message = create_grant_message(pid, MESSAGE_SIZE)
+    data = grant_message.encode()
 
-        # enter critical section and send permission to client
-        critical_sem.acquire()
+    # enter critical section and send permission to client
 
-        # write grant permission on log
-        log_sem.acquire()
-        write_to_log(data, log_file)
-        log_sem.release()
+    # write grant permission on log
+    log_sem.acquire()
+    write_to_log(data, log_file)
+    log_sem.release()
 
-        client.send(data)
-
-    # process the RELEASE message
-    if message_type == 3:
-        # compute statistics
-        access_stats[pid] = access_stats.get(pid, 0) + 1
-        # free critical section
-        critical_sem.release()
+    client.send(data)
 
 
 if __name__ == "__main__":
